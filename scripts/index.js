a2b652c194f1511efeafd01ca6917b65ce449ba7
Dmitry Konychev <dalexeych@gmail.com>
2022-04-02 19:28:22 +0300

    feat: first section on mobile. js added

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
-let swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 0,
+  speed: 800,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
