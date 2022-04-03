3c9a30cbaf2a5119079fb15d4cb55f09500de89c
Dmitry Konychev <dalexeych@gmail.com>
2022-04-03 18:28:38 +0300

    feat: responsive section 1 first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// initializing swiper for section "about"
 const aboutSwiper = new Swiper(".about", {
   slidesPerView: "auto",
   centeredSlides: true,
@@ -11,12 +12,29 @@ const aboutSwiper = new Swiper(".about", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: "auto",
+      spaceBetween: 0
+    },
+    481: {
+      slidesPerView: "auto",
+      centeredSlides: false,
+      spaceBetween: 0
+    },
+    1024: {
+      initialSlide: 1,
+      slidesPerView: "auto",
+      spaceBetween: 0
+    }
+  },
 });
 
 const aboutNavButtons = Array.from(document.querySelectorAll('.about__button'));
 
 // listner for changing aboutSwiper object
 const tabletMedia = window.matchMedia("screen and (min-width: 1024px)");
+
 const handleTablet = (med) => {
   if (med.matches){
     aboutNavButtons.forEach(elem => {elem.classList.remove('about__button_hidden')});
