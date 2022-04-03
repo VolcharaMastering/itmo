912a47070ec92622e2202cb7b6cbf6070e6fe4ac
Dmitry Konychev <dalexeych@gmail.com>
2022-04-03 14:17:17 +0300

    feat: base js media structure

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,14 @@ const cardsSwiper = new Swiper(".about", {
     clickable: true,
   },
 });
+
+const tabletMedia = window.matchMedia("(min-width: 481px)");
+const handleTablet = (med) => {
+  if (med.matches){
+    console.log('RAZ')
+  } else {
+    console.log('DWA')
+  }
+}
+handleTablet(tabletMedia);
+tabletMedia.addListener(handleTablet);
